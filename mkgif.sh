bdaef9f894c570f02c64f7b0165cfe2c2fa7603a
slg74 <scottghome@scottgs-MBP.lan>
2024-06-21 08:55:59 -0500

    update gif maker

--- mkgif.sh
+++ mkgif.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# REQUIREMENTS - ffmpeg, gifsicle
+# Linux: sudo apt-get install ffmpeg gifsicle -y
+# MacOS: brew install ffmpeg gifsicle
+
 # convert .mov to .gif
 ffmpeg -i sgr_A.mov -pix_fmt rgb8 -r 10 sgr_A.gif && gifsicle -03 sgr_A.gif -o sgr_A.gif
